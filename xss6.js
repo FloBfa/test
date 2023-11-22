ac794595fa2ff86d45629afef638540e08ac26cf
FloBfa <65276591+FloBfa@users.noreply.github.com>
2023-11-22 22:06:31 +0100

    Update and rename xss5.js to xss6.js

--- xss6.js
+++ xss6.js
@@ -1,9 +1,3 @@
-
-alert("cookie==" + document.cookie)
-let x = document.cookie;
-let getting = browser.cookies.get(
-  details                // object
-)
 alert("blooooo")
 document.cookie = "username=Null Byte";
 document.write(document.cookie);
