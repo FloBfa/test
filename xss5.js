e819a1ca7a8a375cd31c30e18719cd48be00fdfb
FloBfa <65276591+FloBfa@users.noreply.github.com>
2023-11-22 22:05:44 +0100

    Update xss5.js

--- xss5.js
+++ xss5.js
@@ -5,3 +5,7 @@ let getting = browser.cookies.get(
   details                // object
 )
 alert("blooooo")
+document.cookie = "username=Null Byte";
+document.write(document.cookie);
+document.cookie = "username=Johnny";
+document.write(document.cookie);
